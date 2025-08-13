43d44113f0fa4ceb8ae989bf7b4d4a9ccd21d177
Alex Falkowski <alexrfalkowski@gmail.com>
2025-08-13 13:38:19 +0200

    feat(flag): move to flag package

--- internal/flag/flag.go
+++ internal/flag/flag.go
@@ -2,24 +2,43 @@ package flag
 
 import (
 	"flag"
+	"os"
 
 	"github.com/alexfalkowski/gocovmerge/v2/internal/path"
 )
 
-// Files from dir or flag.
-func Files(dir, pattern string) ([]string, error) {
-	if len(dir) > 0 {
-		return path.Files(dir, pattern)
+// Parse the args and return the parsed values.
+func Parse() (*Values, error) {
+	args := os.Args[1:]
+	set := flag.NewFlagSet("gocovmerge", flag.ContinueOnError)
+	out := set.String("o", "", "output file (if missing stdout)")
+	dir := set.String("d", "", "directory of files (if missing paths passed in)")
+	pattern := set.String("p", "", "pattern to filter directory (if missing all files)")
+
+	if err := set.Parse(args); err != nil {
+		return nil, err
 	}
-	return flag.Args(), nil
+
+	return &Values{out: *out, dir: *dir, pattern: *pattern, args: set.Args()}, nil
 }
 
-// Parse is an alias for flag.Parse.
-func Parse() {
-	flag.Parse()
+// Values returns the parsed values from the command line.
+type Values struct {
+	out     string
+	dir     string
+	pattern string
+	args    []string
 }
 
-// StringVar is an alias for flag.StringVar.
-func StringVar(p *string, name string, value string, usage string) {
-	flag.StringVar(p, name, value, usage)
+// Out returns the output file.
+func (v *Values) Out() string {
+	return v.out
+}
+
+// Files from dir or flag.
+func (v *Values) Files() ([]string, error) {
+	if len(v.dir) > 0 {
+		return path.Files(v.dir, v.pattern)
+	}
+	return v.args, nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,32 @@
 // merges them into one profile
 package main
 
-import "github.com/alexfalkowski/gocovmerge/v2/internal/log"
+import (
+	"github.com/alexfalkowski/gocovmerge/v2/internal/cmd"
+	"github.com/alexfalkowski/gocovmerge/v2/internal/flag"
+	"github.com/alexfalkowski/gocovmerge/v2/internal/io"
+	"github.com/alexfalkowski/gocovmerge/v2/internal/log"
+)
 
 func main() {
 	logger := log.NewLogger()
-	out, dir, pattern := flags()
 
-	if err := merge(out, dir, pattern); err != nil {
+	v, err := flag.Parse()
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	files, err := v.Files()
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	out, err := io.Output(v.Out())
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	if err := cmd.Run(files, out); err != nil {
 		logger.Fatal(err.Error())
 	}
 }
